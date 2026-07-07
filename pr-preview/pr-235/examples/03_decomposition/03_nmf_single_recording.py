3accc275c7d7d1e033e504d3de113eeac871769d
FelipeCybis <FelipeCybis@users.noreply.github.com>
2026-07-07 10:36:22 +0000

    deploy: confusius-tools/confusius@e5fcc606d708e8f7474771b8a814d23f64596f8f

--- pr-preview/pr-235/examples/03_decomposition/03_nmf_single_recording.py
+++ pr-preview/pr-235/examples/03_decomposition/03_nmf_single_recording.py
@@ -1,4 +1,5 @@
-# %% [markdown] # NMF on a single fUSI recording
+# %% [markdown]
+# # NMF on a single fUSI recording
 #
 # This example shows how to use non-negative matrix factorization (NMF) to decompose a
 # fUSI recording into non-negative spatial maps and their associated non-negative time
