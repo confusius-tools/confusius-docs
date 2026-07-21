b44ad1d32dc7cbb6df94f1d81db81921d344c9d1
FelipeCybis <FelipeCybis@users.noreply.github.com>
2026-07-21 00:15:33 +0000

    deploy: confusius-tools/confusius@88766c6df37ee993043a960c4a5348de44607681

--- pr-preview/pr-263/examples/_built/other/plot_surface.py
+++ pr-preview/pr-263/examples/_built/other/plot_surface.py
@@ -1,6 +1,16 @@
+import numpy as np
+
 import confusius as cf
 
 atlas = cf.datasets.fetch_brainglobe_atlas("allen_mouse_25um")
+
+
+rois = set(np.unique(atlas.annotation)) - {0, 997}
+
 viewer, _ = cf.plotting.plot_napari(atlas.atlas.reference)
+cf.plotting.plot_atlas_mesh(atlas, rois, viewer=viewer)
+viewer, layer = cf.plotting.plot_surface(
+    atlas.atlas.get_mesh("VISp"), colormap="magenta", viewer=viewer
+)
 # The mesh, layer name, and color are all pulled from the atlas region.
 atlas.atlas.plot.mesh("root", viewer=viewer)
